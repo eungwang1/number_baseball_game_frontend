eb04674d5fc615fa4852a8a92c5ee77720a01a0a
eungwang1 <scm323@naver.com>
2023-08-14 21:01:38 +0900

    Feat: waiting-room page 파일생성

---
+++ src/components/waiting-room/WaitingRoomComponent.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const WaitingRoomComponentBlock = styled.div``;
+
+interface WaitingRoomComponentProps {}
+
+const WaitingRoomComponent: React.FC<WaitingRoomComponentProps> = () => {
+  return (
+    <WaitingRoomComponentBlock>
+      {/* 컴포넌트의 내용을 여기에 작성하세요. */}
+    </WaitingRoomComponentBlock>
+  );
+};
+
+export default WaitingRoomComponent;
